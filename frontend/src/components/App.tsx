1e15940ef038cc72d16681fb516f9655e0f80e01
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-22 02:43:07 +0900

    frontend: React with Axios

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Problems } from "./Problems";
+import { Records } from "./Records";
+
+import "../styles/layout.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Problems />
+      <Records />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Records.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+
+import "../styles/layout.css";
+
+export function Records() {
+  const [records, setRecords] = React.useState(null);
+
+  return (
+    <div className="records a">
+      <button className="button" onClick={() => getRecords(setRecords)}>
+        Get Records
+      </button>
+
+      <p>{records}</p>
+    </div>
+  );
+}
+
+function getRecords(props: any) {
+  const url = "http://127.0.0.1:8000/records";
+
+  axios
+    .get(url)
+    .then((res) => {
+      props.setRecords(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
