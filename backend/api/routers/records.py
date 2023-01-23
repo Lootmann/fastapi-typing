3d20e4896bf94423c83294b07b0f45b88764a870
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-23 21:58:44 +0900

    backend: impl api/routers/records

--- backend/api/routers/records.py
+++ backend/api/routers/records.py
@@ -11,3 +11,13 @@ def all_records():
 @router.get("/records/{record_id}")
 def all_records(record_id: int):
     return {"record_id": record_id}
+
+
+@router.post("/records")
+def create_record():
+    return {"msg": "create a record"}
+
+
+@router.delete("/records/{record_id}")
+def delete_record(record_id: int):
+    return {"delete": record_id}
