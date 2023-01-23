d6ef51242d07924485a7870e8b7a8fda6ca4bbb1
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-23 22:38:01 +0900

    backend: problems - apply schmeas to routers/problems

--- backend/api/routers/problems.py
+++ backend/api/routers/problems.py
@@ -1,13 +1,17 @@
+from typing import List
+
 from fastapi import APIRouter
+
+import api.schemas.problem as problem_schema
 
 router = APIRouter()
 
 
-@router.get("/problems")
+@router.get("/problems", response_model=List[problem_schema.Problem])
 def all_problems():
     return [
-        {"id": 1, "sentence": "very first"},
-        {"id": 2, "sentence": "whe hello friends :^)"},
+        problem_schema.Problem(id=1, sentence="very first"),
+        problem_schema.Problem(id=2, sentence="why hello friends :^)"),
     ]
 
 
@@ -16,16 +20,16 @@ def problems_by_id(problem_id: int):
     return {"problem_by_id": problem_id}
 
 
-@router.post("/problems")
-def create_problem():
-    return {"create": "create a problem"}
+@router.post("/problems", response_model=problem_schema.ProblemCreateResponse)
+def create_problem(problem_body: problem_schema.ProblemCreate):
+    return problem_schema.ProblemCreateResponse(id=1, **problem_body.dict())
 
 
-@router.put("/problems/{problem_id}")
-def update_problem(problem_id: int):
-    return {"update": problem_id}
+@router.put("/problems/{problem_id}", response_model=problem_schema.ProblemCreateResponse)
+def update_problem(problem_id: int, problem_body: problem_schema.ProblemCreate):
+    return problem_schema.ProblemCreateResponse(id=problem_id, **problem_body.dict())
 
 
-@router.delete("/problems/{problem_id}")
-def delete_problem(problem_id: int):
-    return {"delete": problem_id}
+@router.delete("/problems/{problem_id}", response_model=None)
+def delete_problem():
+    return
---
+++ backend/api/schemas/problem.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+
+
+class ProblemBase(BaseModel):
+    sentence: str | None = Field("", example="typing sentence")
+
+
+class ProblemCreate(ProblemBase):
+    pass
+
+
+class ProblemCreateResponse(ProblemCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Problem(ProblemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
