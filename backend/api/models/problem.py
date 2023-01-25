ed32dc8c888b672d46d6ca9195d4a76cdf86487e
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-26 05:47:21 +0900

    backend: api.models.problem create RecordModel

--- backend/api/models/problem.py
+++ backend/api/models/problem.py
@@ -1,6 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import Column, Integer, String, DateTime
 
 from api.db import Base
 
@@ -10,3 +8,12 @@ class Problem(Base):
 
     id = Column(Integer, primary_key=True)
     sentence = Column(String(1024))
+
+
+class Record(Base):
+    __tablename__ = "records"
+
+    id = Column(Integer, primary_key=True)
+    actual_typing = Column(String(9999))
+    duration = Column(Integer)
+    registered_at = Column(DateTime)
