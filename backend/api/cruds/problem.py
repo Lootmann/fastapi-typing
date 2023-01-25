4556ff1b4a7b8d96fcf837419ba4ec517da6e677
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-26 03:30:07 +0900

    backend: fix typo

--- backend/api/cruds/problem.py
+++ backend/api/cruds/problem.py
@@ -4,7 +4,7 @@ import api.models.problem as problem_model
 import api.schemas.problem as problem_schema
 
 
-async def create_task(
+async def create_problem(
     db: AsyncSession, problem_create: problem_schema.ProblemCreate
 ) -> problem_model.Problem:
     problem = problem_model.Problem(**problem_create.dict())
